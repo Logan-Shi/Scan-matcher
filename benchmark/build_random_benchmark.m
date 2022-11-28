fe09ea3db4e1d1e55466aaab975885e3d026e122
ssz990220 <ssz990220@sjtu.edu.cn>
2022-11-28 16:34:32 +0800

    create data dir if not exist when creating dataset

--- benchmark/build_random_benchmark.m
+++ benchmark/build_random_benchmark.m
@@ -8,12 +8,15 @@ center = mean(src,2);
 src = src-repmat(center,[1,batch_size]);
 scale = max(sqrt(sum(src.^2)));
 src = src/scale;
-for outliers_per = [95]
+for outliers_per = [0 20 60 70 80 90 95 96 97 98 99]
     for i = 1:100
         R = randRotation();
         dst = R*src;
         noise = 0.001;
         [dst,outliers] = addNoise(dst,noise,outliers_per/100,1);
+        if ~exist([surfix, num2str(outliers_per)],'dir')
+            mkdir([surfix, num2str(outliers_per)])
+        end
         save([surfix, num2str(outliers_per),'/',num2str(i),'_dst.mat'],'dst')
         save([surfix, num2str(outliers_per),'/',num2str(i),'_R.mat'],'R')
     end
